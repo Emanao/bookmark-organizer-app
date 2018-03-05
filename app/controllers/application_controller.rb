f7c63983c97aebc28564a69e045daa62e537170b
github emanao <github emanao@me.com>
2018-03-05 14:14:56 -0500

    application controller configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < Sinatra::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "bookmark_secret_key"
+      end
+    
     get "/" do
-        "Welcome to the Bookmark Organizer"
+        erb :index
     end
 end
