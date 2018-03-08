8981462f8689ddace90e62e7cd2e3e4abd42d12c
github emanao <github emanao@me.com>
2018-03-08 11:45:00 -0500

    tags show action done. List of bookmarks related to a tag are shown.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,11 @@
 class TagsController < ApplicationController
+    get '/tags' do
+        "Welcome to the Bookmark Organizer - Tags index" 
+    end
+    #show
+    get '/tags/:id' do
+        @tag=current_user.tags.find_by(id: params[:id])
+        erb :'tags/show'
+    end
 
 end
